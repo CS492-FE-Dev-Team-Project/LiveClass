30c87aef09a3a7e83c8c96cc3f8df924d94906bf
JungInRhee <ysrheee@kaist.ac.kr>
2021-11-04 19:14:36 +0900

    #4/add-express-middlewares-and-health-check

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import { Server as SocketIOServer } from 'socket.io';
 
 import config from './config';
 import { expressLoader, ioLoader, ormLoader } from './loader';
+import Logger from './loader/logger';
 
 class Server {
   public static async start(): Promise<void> {
@@ -17,8 +18,7 @@ class Server {
     await ormLoader();
 
     server.listen(config.HTTP_PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server running on port ${config.HTTP_PORT}`);
+      Logger.info(`Server running on port ${config.HTTP_PORT}`);
     });
   }
 }
