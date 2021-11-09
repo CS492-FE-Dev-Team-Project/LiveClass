b0e6cbc40e0aff73c318ed897892fb2a9617cdb7
JungInRhee <ysrheee@kaist.ac.kr>
2021-11-09 16:57:33 +0900

    Fix eslintrc.js no-shadow error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
-    'no-shadow': 2,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
     'react/prop-types': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
