94ea515f957b69f94d5a4c0ca716cc7eef8242ee
jhlee98 <16jhlee@gmail.com>
2021-11-14 14:37:02 +0900

    #14/Show quiz modal

--- client/src/components/quiz/quiz.tsx
+++ client/src/components/quiz/quiz.tsx
@@ -11,12 +11,36 @@ import {
   Button
 } from '@chakra-ui/react';
 
-import { FormControl, FormLabel } from '@chakra-ui/form-control';
-
+import { Stack } from '@chakra-ui/layout';
+import { Radio, RadioGroup } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/input';
 
 const QuizUser = ({ quiz, disclosure }: any) => {
   const { isOpen, onOpen, onClose } = disclosure;
+  const [value, setValue] = React.useState('1');
+  if (quiz.quiztype) {
+    return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{quiz.quizTitle}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box>{quiz.quizContents}</Box>
+            <Input type="text" placeholder="Write answer here" />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Submit
+            </Button>
+            <Button colorScheme="red" onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  }
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -24,8 +48,22 @@ const QuizUser = ({ quiz, disclosure }: any) => {
         <ModalHeader>{quiz.quizTitle}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Box>{quiz.quizContents}</Box>
-          <Input type="text" placeholder="Write answer here" />
+          <RadioGroup onChange={setValue} value={value}>
+            <Stack direction="column">
+              <Radio value={quiz.quizOption[0]}>
+                {quiz.quizAnswerOption[0]}
+              </Radio>
+              <Radio value={quiz.quizOption[1]}>
+                {quiz.quizAnswerOption[1]}
+              </Radio>
+              <Radio value={quiz.quizOption[2]}>
+                {quiz.quizAnswerOption[2]}
+              </Radio>
+              <Radio value={quiz.quizOption[3]}>
+                {quiz.quizAnswerOption[3]}
+              </Radio>
+            </Stack>
+          </RadioGroup>
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="blue" mr={3}>
--- client/src/pages/quizTestPage.tsx
+++ client/src/pages/quizTestPage.tsx
@@ -9,23 +9,25 @@ const QuizTestPage = (): React.ReactElement<any, any> => {
   const disclosure = useDisclosure();
   const { isOpen, onOpen, onClose } = disclosure;
   const quiz = {
-    quizTitle: 'Addition Quiz',
-    quizContents: 'What is 1+1?',
+    quizTitle: 'What is 1+1?',
     quizAnswer: '1'
   };
   const createquiz = {
+    quiztype: 1,
     quizTitle: 'Problem Description',
     quizAnswer: 'Answer'
   };
   const createquiz2 = {
+    quiztype: 0,
     quizTitle: 'Problem Description',
     quizOption: ['A', 'B', 'C', 'D'],
     quizAnswerOption: ['1', '2', '3', '4']
   };
   return (
     <>
-      <Button onClick={onOpen}>Create Quiz</Button>
-      <CreateQuiz quiz={createquiz} disclosure={disclosure} />
+      <Button onClick={onOpen}>Open Quiz</Button>
+      <QuizUser quiz={createquiz2} disclosure={disclosure} />
+      {/* <CreateQuiz quiz={createquiz} disclosure={disclosure} /> */}
       {/* <CreateQuizMultiple quiz={createquiz2} disclosure={disclosure} /> */}
     </>
   );
