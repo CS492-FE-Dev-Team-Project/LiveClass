eda13379143f8ee1bdffd08f7e898993308572b9
JungInRhee <ysrheee@kaist.ac.kr>
2021-11-23 20:12:27 +0900

    #48/Create Login Modal

---
+++ client/src/components/loginModal.tsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  Button,
+  Image,
+  ModalOverlay,
+  ModalHeader,
+  Spinner
+} from '@chakra-ui/react';
+
+import NaverLoginButtonAsset from '../assets/naver_login_kor.png';
+import UserContext from '../context/user/userContext';
+import { UserLoadStatus } from '../context/user/userProvider';
+
+const NaverLoginButton = ({ ...props }) => {
+  return (
+    <Button w={400} h={100} {...props}>
+      <Image src={NaverLoginButtonAsset} w="full" h="full" />
+    </Button>
+  );
+};
+
+const LoginModal = () => {
+  const { status } = useContext(UserContext);
+
+  return (
+    <Modal
+      isOpen={status !== UserLoadStatus.LOADED}
+      onClose={() => {
+        console.log();
+      }}
+    >
+      <ModalOverlay />
+      {status === UserLoadStatus.LOADING ? (
+        <Spinner />
+      ) : (
+        <ModalContent>
+          <ModalHeader>Login</ModalHeader>
+          <ModalBody>
+            <NaverLoginButton
+              onClick={() => {
+                window.location.pathname = '/api/auth/naver';
+              }}
+            />
+          </ModalBody>
+        </ModalContent>
+      )}
+    </Modal>
+  );
+};
+
+export default LoginModal;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import UserProvider from './context/user/userProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
