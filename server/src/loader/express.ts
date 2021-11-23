bd8fdc9eecabafc64c0ca58b9828a0183d2fe483
JungInRhee <ysrheee@kaist.ac.kr>
2021-11-23 16:57:43 +0900

    #47/Add client hosting for non-api prefix http request

--- server/src/loader/express.ts
+++ server/src/loader/express.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan, { StreamOptions } from 'morgan';
 import passport from 'passport';
-import myPassport from '../passport';
+import path from 'path';
 
+import myPassport from '../passport';
 import serverRoute from '../routes';
 import Logger from './logger';
 
@@ -27,6 +28,8 @@ export default (app: express.Application, sessionMiddleware: any) => {
   app.use(passport.session());
   myPassport();
 
+  app.use(express.static(path.join(__dirname, '../../client')));
+
   app.get('/', (req, res) => {
     res.send('LiveClass Main Server!!').status(200);
   });
@@ -34,9 +37,8 @@ export default (app: express.Application, sessionMiddleware: any) => {
   // TODO: Add Authentication Middlewares
 
   app.use('/api', serverRoute());
-
-  app.use('/*', (req, res) => {
-    res.send('404 Bad Request: Invalid Url').status(404);
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/index.html'));
   });
 
   // TODO: Add Error Handling middlewares
