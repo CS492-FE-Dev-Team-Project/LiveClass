6b897ffc5d980843ca3025425069a8a971f1b374
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-04 02:03:15 +0900

    #109/Sync Chat Component to LiveChat API

--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -5,11 +5,10 @@ import ChatMessage from './chatMessage';
 import ChatInput from './chatInput';
 import Header from './common/Header';
 
-import { MarkerType } from '../types';
+import { MarkerType, TextMessageResponse } from '../types';
 
 import { useSocket } from '../context/socket';
 import useMe from '../hooks/useMe';
-import dummyMessages from '../data/chatMessages'; // 🐛 Dummy message - call DB API to get real data
 
 enum ChatMode {
   Marker = 'Marker',
@@ -26,27 +25,33 @@ type ChatStatus =
 
 interface ChatProps {
   hasHeader: boolean;
-  room: string;
+  classUuid: string;
+  lectureId: number;
   customHeader?: string;
 }
 
 // 🚨 Possible duplicate with 'interface ChatMessageProps' in 'chatMessage.tsx'
 interface Message {
+  messageId: number;
   userName: string;
   message: string;
   time: string;
-  id: number;
   isMy: boolean;
 }
 
-const Chat = ({ hasHeader, room, customHeader = '' }: ChatProps) => {
+const Chat = ({
+  hasHeader,
+  classUuid,
+  lectureId,
+  customHeader = ''
+}: ChatProps) => {
   const { socket, connected } = useSocket();
-  const [messages, setMessages] = useState<Array<Message>>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [chatStatus, setChatStatus] = useState<ChatStatus>({
     chatMode: ChatMode.Live
   });
   const currentMarkerId = useRef<number>(-1);
-  const { userName: currentUserName } = useMe();
+  const { id: myId, userName: myName } = useMe();
 
   // Set Chat header title
   const pickHeader = (chatStat: ChatStatus) => {
@@ -74,25 +79,28 @@ const Chat = ({ hasHeader, room, customHeader = '' }: ChatProps) => {
         // 🐛 (API) Fetch timeMarker thread messages
         currentMarkerId.current = markerId;
         // setChatMode(markerType as number);
-        setMessages(dummyMessages.slice(markerId * 3, markerId * 3 + 3));
+        // setMessages(dummyMessages.slice(markerId * 3, markerId * 3 + 3));
       }
     );
 
     // Live chat event - get live chat message
-    socket?.on('ChatTextMessage', (payload: string) => {
-      const { dateStr, textMessage, chatUserName, isMy } = JSON.parse(payload);
-
+    socket?.on('LiveChatTextMessage', ({ message, status }) => {
+      const {
+        dateStr,
+        senderId,
+        senderName,
+        text,
+        messageId
+      }: TextMessageResponse = message;
       const dateObj = new Date(dateStr);
-
-      const dummyMessageObj: Message = {
-        id: 999, // 🐛 Get rid of id?
-        userName: chatUserName,
-        message: textMessage,
+      const messageObj: Message = {
+        messageId,
+        userName: senderName,
+        message: text,
         time: `${dateObj.getHours()}:${dateObj.getMinutes()}`,
-        isMy
+        isMy: senderId === myId
       };
-
-      setMessages(arr => [...arr, dummyMessageObj]);
+      setMessages(arr => [...arr, messageObj]);
     });
   }, [connected]);
 
@@ -104,24 +112,17 @@ const Chat = ({ hasHeader, room, customHeader = '' }: ChatProps) => {
     setMessages([]); // 🐛 (API?) Fetch Live chat message
   };
 
-  const createMessage = (message: string) => {
+  const createMessage = (text: string) => {
     // 🐛 (API) Create message
     // Providing info : message (string)
-    const dummyMessageObj = {
-      id: 999,
-      userName: 'MyName',
-      message,
-      time: '00:00',
-      isMy: true
-    };
 
     // Send message
     socket?.emit(
-      'ChatTextMessage',
+      'LiveChatTextMessage',
       JSON.stringify({
-        classUuid: room,
-        currentUserName,
-        textMessage: message
+        classUuid,
+        lectureId,
+        text
       })
     );
 
@@ -156,9 +157,9 @@ const Chat = ({ hasHeader, room, customHeader = '' }: ChatProps) => {
         </Header>
       )}
       <Flex w="280px" overflowY="auto" pb={3} pt={3} flexDir="column" h="full">
-        {messages.map(({ userName, message, time, id, isMy }) => (
+        {messages.map(({ userName, message, time, messageId, isMy }) => (
           <ChatMessage
-            key={id}
+            key={messageId}
             userName={userName}
             message={message}
             time={time}
