845514da3230b50a642b72e8c0d057bd727c8be7
hy2850 <hy2850@naver.com>
2021-12-04 00:59:53 +0900

    #113/Fix leftmenuData to only keep default data

---
+++ client/src/data/leftmenuData.ts
@@ -0,0 +1,21 @@
+import { TabSegment, TabType } from '../types';
+
+const menus: Array<TabSegment> = [
+  {
+    tabTitle: 'CS330',
+    tabContents: [
+      {
+        tabName: 'Notice',
+        type: TabType.NOTICE,
+        message: ''
+      },
+      {
+        tabName: 'Materials',
+        type: TabType.NOTICE,
+        message: ''
+      }
+    ]
+  }
+];
+
+export default menus;
