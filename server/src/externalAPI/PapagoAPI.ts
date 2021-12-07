bfec9ec68910514652ab2f9c33a5e74a6ab0b7b2
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-07 15:06:49 +0900

    #119/Add papago api calls

--- server/src/externalAPI/PapagoAPI.ts
+++ server/src/externalAPI/PapagoAPI.ts
@@ -1,41 +1,57 @@
 import axios from 'axios';
-import * as dotenv from 'dotenv';
+import config from '../config';
+import {
+  Language,
+  PapagoLanguageDetectionResponse,
+  PapagoTranslateResponse
+} from '../types';
 
-dotenv.config();
+const translate = async (source: Language, target: Language, text: string) => {
+  if (source === target) return { result: text, status: 201 };
 
-const callPapagoTranslation = (
-  source: string,
-  target: string,
-  text: string
-) => {
-  let translatedString: string = '';
-  let status;
-  axios
-    .request({
-      url: 'https://openapi.naver.com/v1/papago/n2mt',
-      method: 'POST',
-      data: {
-        source,
-        target,
-        text
-      },
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'X-Naver-Client-Id': process.env.NAVER_PAPAGO_CLIENT_ID!,
-        'X-Naver-Client-Secret': process.env.NAVER_PAPAGO_CLIENT_SECRET!
-      }
-    })
-    .then(res => {
-      status = 200;
-      translatedString = res.data;
-    })
-    .catch(err => {
-      status = 400; // FIX
-      if (err.response) console.log('res error : ', err.response.data);
-      if (err.request) console.log('req error : ', err.request.data);
-    });
+  const { data, status } = await axios.request({
+    url: 'https://openapi.naver.com/v1/papago/n2mt',
+    method: 'POST',
+    data: {
+      source,
+      target,
+      text
+    },
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      'X-Naver-Client-Id': config.auth.naver.clientId,
+      'X-Naver-Client-Secret': config.auth.naver.clientSecret
+    }
+  });
 
-  return { translatedString, status };
+  const {
+    message: { result: translatedText }
+  }: PapagoTranslateResponse = data;
+
+  return { result: translatedText, status };
 };
 
-export default callPapagoTranslation;
+// curl "https://openapi.naver.com/v1/papago/detectLangs" \
+//     -d "query=만나서 반갑습니다." \
+//     -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+//     -H "X-Naver-Client-Id: MsZ7xuiNrx_i7M53OKC7" \
+//     -H "X-Naver-Client-Secret: ck0NrxrKXT" -v
+
+const detectLanguage = async (query: string) => {
+  const { data } = await axios.request({
+    url: 'https://openapi.naver.com/v1/papago/detectLangs',
+    method: 'POST',
+    data: { query },
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      'X-Naver-Client-Id': config.auth.naver.clientId,
+      'X-Naver-Client-Secret': config.auth.naver.clientSecret
+    }
+  });
+
+  const { langCode }: PapagoLanguageDetectionResponse = data;
+
+  return { src: langCode };
+};
+
+export { detectLanguage, translate };
