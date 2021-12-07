aee947c1a3c2337b601e57532c76cf21ae1a6913
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-07 15:07:57 +0900

    #119/Add authentication middleware

--- server/src/routes/translateRoute/index.ts
+++ server/src/routes/translateRoute/index.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { detectLanguage, translate } from '../../externalAPI/PapagoAPI';
 import Logger from '../../loader/logger';
+import { authenticateUser } from '../../passport';
 import { TranslateRequestInterface } from '../../types';
 
 export default (app: Router) => {
   const translateRouter = Router();
   app.use('/translate', translateRouter);
 
-  translateRouter.post('/', async (req, res) => {
+  translateRouter.post('/', authenticateUser, async (req, res) => {
     try {
       const { translateArray, target }: TranslateRequestInterface = req.body;
 
