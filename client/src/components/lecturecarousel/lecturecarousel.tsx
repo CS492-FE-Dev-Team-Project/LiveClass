597ef9fa4754c5ea36ba392733b78b6391729008
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-08 14:34:10 +0900

    #142/Impl thumbnail

--- client/src/components/lecturecarousel/lecturecarousel.tsx
+++ client/src/components/lecturecarousel/lecturecarousel.tsx
@@ -1,5 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/swiper.scss';
@@ -8,31 +7,12 @@ import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
 import './carousel.css';
 
-import { getPlayListItems } from '../common/playlist';
-
 import LectureCard from './lecturecard';
+import { Lecture } from '../../types';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const LectureCarousel = ({ classUuid, memberType, lectureList }: any) => {
-  const [firstVidIds, setFirstVidIds] = useState<string[]>([]);
-
-  const getFirstVideoIds = async () => {
-    const ids: string[] = [];
-    lectureList.forEach(({ playlist }: any) => {
-      getPlayListItems(playlist).then(data => {
-        ids.push(data.items[0].snippet.resourceId.videoId);
-      });
-    });
-    return ids;
-  };
-
-  useEffect(() => {
-    getFirstVideoIds().then(res => {
-      setFirstVidIds(res);
-    });
-  }, []);
-
   const pagination = {
     clickable: true,
     renderBullet: (index: any, className: any) => {
@@ -51,14 +31,17 @@ const LectureCarousel = ({ classUuid, memberType, lectureList }: any) => {
         pagination={pagination}
       >
         {lectureList.map(
-          (
-            { id: lectureId, lectureDate, lectureName, LiveStatus }: any,
-            idx: number
-          ) => (
+          ({
+            id: lectureId,
+            lectureDate,
+            lectureName,
+            LiveStatus,
+            playlist
+          }: Lecture) => (
             <SwiperSlide>
               <LectureCard
                 lectureNum={lectureId}
-                youtubeID={firstVidIds.length > idx ? firstVidIds[idx] : ''}
+                youtubePlayList={playlist}
                 date={lectureDate.slice(0, 10)}
                 to={`/class/${classUuid}/${memberType}/${lectureId}`}
                 key={lectureId}
