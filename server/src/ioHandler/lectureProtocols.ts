1a8e1d94c354bdba7c413109ef487f3245434f67
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-08 13:34:46 +0900

    #144/Add Error Handling at all event handlers

--- server/src/ioHandler/lectureProtocols.ts
+++ server/src/ioHandler/lectureProtocols.ts
@@ -7,72 +7,91 @@ import { CustomSocket, InLectureRequestInterface } from '../types';
 const OnJoinLecture =
   (socket: CustomSocket, classManager: ClassManager) =>
   async ({ classUuid, lectureId }: InLectureRequestInterface) => {
-    const cls: Class = await classManager.getOrCreateClass(classUuid);
-    const lecture: Lecture = cls.getLectureById(lectureId);
+    try {
+      const cls: Class = await classManager.getOrCreateClass(classUuid);
+      const lecture: Lecture = cls.getLectureById(lectureId);
 
-    if (lecture.addParticipant(cls.getMemberById(socket.request.user!.id))) {
-      Logger.debug(
-        `Join Lecture:\nLecture: ${JSON.stringify(lecture, null, 2)}`
-      );
-      socket.join(lecture.getSocketRoomName());
-      socket.to(lecture.getSocketRoomName()).emit('JoinLecture', {
-        user: socket.request.user,
-        lecture,
-        status: 200
-      });
-      socket.emit('JoinLecture', {
-        user: socket.request.user,
-        lecture,
-        status: 200
-      });
-    } else {
-      socket.emit('JoinLecture', {
-        user: socket.request.user,
-        lecture,
-        status: 200
-      });
+      if (lecture.addParticipant(cls.getMemberById(socket.request.user!.id))) {
+        Logger.debug(
+          `Join Lecture:\nLecture: ${JSON.stringify(lecture, null, 2)}`
+        );
+        socket.join(lecture.getSocketRoomName());
+        socket.to(lecture.getSocketRoomName()).emit('JoinLecture', {
+          user: socket.request.user,
+          lecture,
+          status: 200
+        });
+        socket.emit('JoinLecture', {
+          user: socket.request.user,
+          lecture,
+          status: 200
+        });
+      } else {
+        socket.emit('JoinLecture', {
+          user: socket.request.user,
+          lecture,
+          status: 200
+        });
+      }
+    } catch (e) {
+      Logger.error(e);
+      socket.emit('JoinLecture', { message: e, status: 400 });
     }
   };
 
 const OnExitLecture =
   (socket: CustomSocket, classManager: ClassManager) =>
   async ({ classUuid, lectureId }: InLectureRequestInterface) => {
-    const cls = await classManager.getOrCreateClass(classUuid);
-    const lecture = cls.getLectureById(lectureId);
+    try {
+      const cls = await classManager.getOrCreateClass(classUuid);
+      const lecture = cls.getLectureById(lectureId);
 
-    const member = lecture.exitParticipant(socket.request.user?.id!);
+      const member = lecture.exitParticipant(socket.request.user?.id!);
 
-    socket.to(lecture.getSocketRoomName()).emit('GetActiveLectureMember', {
-      member,
-      status: 200
-    });
+      socket.to(lecture.getSocketRoomName()).emit('GetActiveLectureMember', {
+        member,
+        status: 200
+      });
+    } catch (e) {
+      Logger.error(e);
+    }
   };
 
 const GetActiveLectureMember =
   (socket: CustomSocket, classManager: ClassManager) =>
   async ({ classUuid, lectureId }: InLectureRequestInterface) => {
-    const cls = await classManager.getOrCreateClass(classUuid);
-    const lecture = cls.getLectureById(lectureId);
+    try {
+      const cls = await classManager.getOrCreateClass(classUuid);
+      const lecture = cls.getLectureById(lectureId);
 
-    socket.emit('GetActiveLectureMember', {
-      members: lecture.getParticipants(),
-      status: 200
-    });
+      socket.emit('GetActiveLectureMember', {
+        members: lecture.getParticipants(),
+        status: 200
+      });
+    } catch (e) {
+      Logger.error(e);
+      socket.emit('GetActiveLectureMember', { message: e, status: 400 });
+    }
   };
 
 const OnSetLectureLiveStatus =
   (socket: CustomSocket, classManager: ClassManager) =>
   async (request: string) => {
-    const { classUuid, lectureId, status } = JSON.parse(request);
-    const cls: Class = await classManager.getOrCreateClass(classUuid);
+    try {
+      const { classUuid, lectureId, status } = JSON.parse(request);
+      const cls: Class = await classManager.getOrCreateClass(classUuid);
 
-    const lecture: Lecture = cls.getLectureById(lectureId);
-    const liveStatus = lecture.setLiveStatus(status);
+      const lecture: Lecture = cls.getLectureById(lectureId);
+      const liveStatus = lecture.setLiveStatus(status);
 
-    socket
-      .to(lecture.getSocketRoomName())
-      .emit('SetLectureLiveStatus', { liveStatus, status: 200 });
-    socket.emit('SetLectureLiveStatus', { liveStatus, status: 200 });
+      socket
+        .to(lecture.getSocketRoomName())
+        .emit('SetLectureLiveStatus', { liveStatus, status: 200 });
+      socket.emit('SetLectureLiveStatus', { liveStatus, status: 200 });
+    } catch (e) {
+      Logger.error(e);
+      socket.emit('SetLectureLiveStatus', { message: e, status: 400 });
+    }
   };
 
 export default {
